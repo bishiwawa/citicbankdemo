f5b9f5b000d7ff3ae82e51d06e4585b9fd4f0f06
wc666888 <1274171949@qq.com>
2020-06-10 23:12:07 +0800

    代码修改

--- hello/src/main/java/com/example/hello/po/Status.java
+++ hello/src/main/java/com/example/hello/po/Status.java
@@ -7,9 +7,11 @@ package com.example.hello.po;
  * @date 2020.6.10
  * */
 public enum Status {
-    OK("成功", 1), VALIDATE_ERROR("验证失败", 2),
-    NO_AUTHORITY("权限不足",3),SERVER_ERROR("服务器异常",4)
-    ,CANCEL_CARD_ERROR("注销银行卡失败",5);
+    OK("成功", 1),
+    VALIDATE_ERROR("验证失败", 2),
+    NO_AUTHORITY("权限不足",3),
+    SERVER_ERROR("服务器异常",4),
+    CANCEL_CARD_ERROR("注销银行卡失败",5);
 
     private String des;
     private int code;
